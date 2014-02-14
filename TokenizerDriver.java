b8a6d45efb2e13fd75fa2874a4d13cef88577281
Brandon S. Kane <me@brandonskane.com>
2014-02-13 22:23:29 -0500

    Style update

--- TokenizerDriver.java
+++ TokenizerDriver.java
@@ -4,17 +4,17 @@ import java.io.IOException;
 import org.apache.commons.io.IOUtils;
 
 /**
- * @author brandonskane
- * a text processing module as follows:
-		A tokenizer according to the following rules:
-		Tokenize all abbreviations containing periods as strings without periods
-		Treat the rest of the punctuation as word separators
-		Lowercase all letters
-
-	  	A stemmer implemented via the Porter Stemmer package: http://tartarus.org/~martin/PorterStemmer/
-		Implement stopword removal using the following stopword list: http://bit.ly/1bqQWaV
-
-		The word list is tokenized first, stopwords are removed, and finally the words are put through the stemmer
+ * @author brandonskane a text processing module as follows: A tokenizer
+ *         according to the following rules: Tokenize all abbreviations
+ *         containing periods as strings without periods Treat the rest of the
+ *         punctuation as word separators Lowercase all letters
+ * 
+ *         A stemmer implemented via the Porter Stemmer package:
+ *         http://tartarus.org/~martin/PorterStemmer/ Implement stopword removal
+ *         using the following stopword list: http://bit.ly/1bqQWaV
+ * 
+ *         The word list is tokenized first, stopwords are removed, and finally
+ *         the words are put through the stemmer
  */
 public class TokenizerDriver {
 
@@ -34,14 +34,14 @@ public class TokenizerDriver {
 		String stopWordsString = processStopWords(stopWordsFileName);
 
 		System.out.println(rawInput);
-		
+
 		String[] seperatedWords = rawInput.split(" ");
 		String[] stopWords = stopWordsString.split("\n");
 
 		Tokenizer tokenizer = new Tokenizer(seperatedWords, stopWords);
 
 		String tokenizedWords = tokenizer.getProcessedWords();
-		
+
 		System.out.println();
 		System.out.println("Text Processing Tokenizer");
 		System.out.println("-------------------------");
@@ -59,8 +59,8 @@ public class TokenizerDriver {
 		}
 		return textToProcess;
 	}
-	
-	static String processStopWords(String fileName) throws IOException{
+
+	static String processStopWords(String fileName) throws IOException {
 		String stopWords;
 		FileInputStream inputStream = new FileInputStream(fileName);
 		try {
@@ -69,6 +69,6 @@ public class TokenizerDriver {
 			inputStream.close();
 		}
 		return stopWords;
-		
+
 	}
 }
